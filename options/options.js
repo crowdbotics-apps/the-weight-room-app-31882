9f60ed398398a29fb8d90c577c9c949b88cfdf39
Crowdbotics <team@crowdbotics.com>
2021-11-09 00:00:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "the_weight_room_app_31882",
+  url: "https://the_weight_room_app_31882.botics.co",
+  api: "https://the_weight_room_app_31882.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
